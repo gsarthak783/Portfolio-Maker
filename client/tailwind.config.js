88e465b44a0a86ec6efdd68f5638ab76c88cae97
Sarthak <gsarthak783@gmail.com>
2024-12-16 17:03:25 +0530

    changes in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,26 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+// module.exports = {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: '#1B1F3B',
+//         text: '#E0E5EC',
+//         accent: '#00B4D8',
+//         secondary: '#FFD166',
+//       },
+//       fontFamily: {
+//         heading: ['Poppins', 'sans-serif'],
+//         body: ['Roboto', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        background: '#1B1F3B',
-        text: '#E0E5EC',
-        accent: '#00B4D8',
-        secondary: '#FFD166',
-      },
-      fontFamily: {
-        heading: ['Poppins', 'sans-serif'],
-        body: ['Roboto', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
