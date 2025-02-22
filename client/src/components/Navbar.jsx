fce9e431532432af6196757dada241589edadf28
Sarthak Goel <gsarthak783@gmail.com>
2025-02-23 01:51:59 +0530

    Removed Experience and Project from Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,51 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import { auth } from '../firebase/Firebase';
-// import { onAuthStateChanged, signOut } from 'firebase/auth';
-
-// const Navbar = () => {
-//   const [user, setUser] = useState(null);
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-//       setUser(currentUser);
-//       // localStorage.setItem('name', currentUser.displayName);
-//     });
-
-//     return () => unsubscribe();
-//   }, []);
-
-//   const logout = async () => {
-//     await signOut(auth);
-//     localStorage.removeItem('name');
-//   };
-
-//   return (
-//     <nav className="bg-slate-700 p-4">
-//       <div className="container mx-auto flex justify-between items-center">
-//         <Link to="/" className="text-white font-bold ">Home</Link>
-//         <div className="flex space-x-4 font-mono items-center">
-//           {user === null ? (
-//             <div className="flex space-x-4">
-//               <Link to="/register" className="bg-blue-500 text-white font-bold px-4 py-2 rounded-lg transition duration-200 hover:bg-blue-600">Register</Link>
-//               <Link to="/login" className="bg-blue-500 text-white font-bold px-4 py-2 rounded-lg transition duration-200 hover:bg-blue-600">Login</Link>
-//             </div>
-//           ) : (
-            
-//             <>
-
-//             </>
-//           )}
-//         </div>
-//         <Link to="/experience" className=" text-white font-bold px-4 py-2 transition duration-200 hover:text-slate-200">Experience</Link>
-//         <Link to="/project" className=" text-white font-bold px-4 py-2 transition duration-200 hover:text-slate-200">Projects</Link>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase/Firebase';
@@ -103,7 +55,7 @@ const Navbar = () => {
               </button> */}
             </>
           )}
-          <Link
+          {/* <Link
             to="/experience"
             className="text-white font-bold text-lg px-4 py-2 transition duration-200 hover:text-slate-200"
           >
@@ -114,7 +66,7 @@ const Navbar = () => {
             className="text-white font-bold text-lg px-4 py-2 transition duration-200 hover:text-slate-200"
           >
             Projects
-          </Link>
+          </Link> */}
         </div>
       </div>
     </nav>
