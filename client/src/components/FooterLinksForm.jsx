9911420bdff6f117f3b22f7406ea343123f4caf6
Sarthak Goel <gsarthak783@gmail.com>
2025-04-21 00:24:58 +0530

    resume added in form

--- client/src/components/FooterLinksForm.jsx
+++ client/src/components/FooterLinksForm.jsx
@@ -98,6 +98,23 @@ const FooterLinksForm = () => {
             )}
           </div>
 
+           {/* Resume */}
+          <div>
+            <label className="block text-gray-700 font-medium">Resume URL</label>
+            <input
+              type="url"
+              {...register("resume", {
+                // required: "Resume URL is required",
+              })}
+              className={`w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black ${
+                errors.resume ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.resume && (
+              <span className="text-red-500 text-sm">{errors.resume.message}</span>
+            )}
+          </div>
+
           {/* Submit Button */}
           <button
             type="submit"
